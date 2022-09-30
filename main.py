813cafec97fbde0c8827c2417f5cbe684556155c
k2supra <andrijkorolevic@gmail.com>
2022-09-30 19:06:54 +0300

    Created new feature/hw_ex_1

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("who hoo")
+print("Введіть цифру 1-7")
+day = int(input("->"))
+
+if day == 1:
+    print("Понеділок")
+
+elif day == 2:
+    print("Вівторок")
+
+elif day == 3:
+    print("Середа")
+
+elif day == 4:
+    print("Четвер")
+
+elif day == 5:
+    print("П'ятниця")
+
+elif day == 6:
+    print("Субота")
+
+elif day == 7:
+    print("Неділя")
+
+else:
+    print("ВВЕДІТЬ ЧИСЛО ВІД 1 ДО 7")
