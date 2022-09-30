f504afd10e916ffd5ec5c1459e3240ebe2f4c0b9
k2supra <andrijkorolevic@gmail.com>
2022-09-30 20:26:25 +0300

    Created new feature/hw_ex_3

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("who hoo")
+print("Введіть число")
+value = int(input("->"))
+
+if value > 0:
+    print("Number is positive")
+
+if value < 0:
+    print("Number is negative")
+
+if value == 0:
+    print("Number is equal to zero")
