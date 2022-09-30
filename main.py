9d3f2fbe2ada26a364a5d1b03be004adc54c1275
k2supra <andrijkorolevic@gmail.com>
2022-09-30 20:30:28 +0300

    Created new feature/hw_ex_4

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("who hoo")
+print("Введіть 1 число")
+val1 = int(input("->"))
+
+print("Введіть 2 число")
+val2 = int(input("->"))
+
+if val1 > val2:
+    print(f"{val1} , {val2}")
+
+elif val1 < val2:
+    print(f"{val2} , {val1}")
+
+else:
+    print(f"{val1} = {val2}")
