e792efa28f4dfe5cfa56f1a1fd69647612bd59e8
k2supra <andrijkorolevic@gmail.com>
2022-09-30 20:23:23 +0300

    Created new feature/hw_ex_2

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-print("who hoo")
+print("Введіть цифру 1-12")
+day = int(input("->"))
+
+if day == 1:
+    print("Січень")
+
+elif day == 2:
+    print("Лютий")
+
+elif day == 3:
+    print("Березень")
+
+elif day == 4:
+    print("Квітень")
+
+elif day == 5:
+    print("Травень")
+
+elif day == 6:
+    print("Червень")
+
+elif day == 7:
+    print("Липень")
+
+elif day == 8:
+    print("Серпень")
+
+elif day == 9:
+    print("Вересень")
+
+elif day == 10:
+    print("Жовтень")
+
+elif day == 11:
+    print("Листопад")
+
+elif day == 12:
+    print("Грудень")
+
+else:
+    print("ВВЕДІТЬ ЧИСЛО ВІД 1 ДО 12")
